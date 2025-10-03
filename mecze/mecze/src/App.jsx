7e69aa074d787972bc04f6d589d9a5f6ef66feaa
k-scibor <uT_3780@zsp3zamosc.pl>
2025-10-03 09:28:42 +0200

    dodano kafelki na wyniki

--- mecze/mecze/src/App.jsx
+++ mecze/mecze/src/App.jsx
@@ -44,6 +44,20 @@ function App() {
                           </table>
                       </div>
                   </div>
+                  <div class="row">
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                      <div class="col-lg-1"></div>
+                  </div>
           </div>
               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
           </body>
